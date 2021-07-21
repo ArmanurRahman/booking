1f5b8da9a6b4544fe77ce35d495e394627016b2e
mubeen <mubeenarman19@gmail.com>
2021-07-22 02:29:01 +0900

    create seed migration

---
+++ migrations/20210721172232_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into rooms (room_name, create_at, update_at) values 
+('General''s Quarters', current_timestamp, current_timestamp),
+('Major''s Suite', current_timestamp, current_timestamp);
---
+++ migrations/20210721172527_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into restrictions (restriction_name, create_at, update_at)
+values('Reservation', current_timestamp, current_timestamp),
+values('Owner Block', current_timestamp, current_timestamp);
