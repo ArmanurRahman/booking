a36baa06d2c4deb91dedfd37fad4fc27cf57adfc
mubeen <mubeenarman19@gmail.com>
2021-07-22 15:41:55 +0900

    update handler test

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -39,6 +39,11 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(app.MailChan)
+
+	listenForMail()
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -61,6 +66,13 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
+}
 func getRoutes() http.Handler {
 
 	mux := chi.NewRouter()
